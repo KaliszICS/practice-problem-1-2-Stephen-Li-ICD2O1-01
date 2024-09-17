85026990c69fc3f0267ff2ca5c385cc623e87b60
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2024-09-17 13:37:01 +0000

    j

--- PP1_2.py
+++ PP1_2.py
@@ -1,25 +1,34 @@
+#def q1():
+name = "Alex"
+print(name)
 
+#def q2():
+num1 = 3
+num2 = 5
+num3 = 2
+print(num1 + num2 + num3)
 
-#Write Assignment code here
+#def q3():
+num1 = 21
+num2 = 4
+print(num1 // num2)
 
-def q1():
+#def q4():
+fracNum1 = 6.3
+fracNum2 = 3.0
+print(fracNum1 / fracNum2)
 
-def q2():
+#def q5():
+word = "Hello World"
+print(word)
+word = "Goodbye World"
+print(word)
 
-def q3():
+#def q6():
+tOrF = "True"
+print(tOrF)
 
-def q4():
-
-def q5():
-
-def q6():
-
-def q7():
-
-q1()
-q2()
-q3()
-q4()
-q5()
-q6()
-q7()
+#def q7():
+num1 = 7
+num2 = 2
+print(num1 % num2)
