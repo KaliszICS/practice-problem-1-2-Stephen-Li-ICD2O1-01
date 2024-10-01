2c85b2bde32eef676f671c5f22b2dffdd6711bbc
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2024-10-01 14:04:49 +0000

    commit

--- PP1_2.py
+++ PP1_2.py
@@ -1,34 +1,42 @@
-#def q1():
-name = "Alex"
-print(name)
+def q1():
+    name = "Alex"
+    print(name)
 
-#def q2():
-num1 = 3
-num2 = 5
-num3 = 2
-print(num1 + num2 + num3)
+def q2():
+    num1 = 3
+    num2 = 5
+    num3 = 2
+    print(num1 + num2 + num3)
 
-#def q3():
-num1 = 21
-num2 = 4
-print(num1 // num2)
+def q3():
+    num1 = 21
+    num2 = 4
+    print(num1 // num2)
 
-#def q4():
-fracNum1 = 6.3
-fracNum2 = 3.0
-print(fracNum1 / fracNum2)
+def q4():
+    fracNum1 = 6.3
+    fracNum2 = 3.0
+    print(fracNum1 / fracNum2)
 
-#def q5():
-word = "Hello World"
-print(word)
-word = "Goodbye World"
-print(word)
+def q5():
+    word = "Hello World"
+    print(word)
+    word = "Goodbye World"
+    print(word)
 
-#def q6():
-tOrF = "True"
-print(tOrF)
+def q6():
+    tOrF = "True"
+    print(tOrF)
 
-#def q7():
-num1 = 7
-num2 = 2
-print(num1 % num2)
+def q7():
+    num1 = 7
+    num2 = 2
+    print(num1 % num2)
+
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
+# q6()
+# q7()
